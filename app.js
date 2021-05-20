e15146df36a0f169bbd2b33dbde08ed86915f0e2
kgovind12 <kgovind@ucdavis.edu>
2021-05-20 16:55:44 -0700

    start css done

---
+++ app.js
@@ -0,0 +1,14 @@
+const playNow = document.getElementById('playNow'), 
+start = document.getElementById('start');
+
+playNow.onclick(directions());
+
+function directions(){
+    document.location.href = 'https://cory-risch-smith.github.io/DES149_Final/directions.html';
+}
+
+start.onclick(startGamelse());
+
+function startGame(){
+document.location.href = 'https://cory-risch-smith.github.io/DES149_Final/game.html';
+}

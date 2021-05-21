c74d110c2355aaf46fb36053aad81f1895ed1490
kgovind12 <kgovind@ucdavis.edu>
2021-05-20 18:28:12 -0700

    changed js again

--- script/script.js
+++ script/script.js
@@ -1,16 +1,13 @@
 const playNow = document.getElementById('playNow'),
   start = document.getElementById('start');
 
-playNow.onclick(directions());
-
-function directions() {
+playNow.addEventListener('click', function(){
   document.location.href =
     'https://cory-risch-smith.github.io/DES149_Final/directions.html';
-}
+});
 
-start.onclick(startGame());
 
-function startGame() {
+start.addEventListener('click', function(){
   document.location.href =
-    'https://cory-risch-smith.github.io/DES149_Final/game.html';
-}
+     'https://cory-risch-smith.github.io/DES149_Final/game.html';
+});

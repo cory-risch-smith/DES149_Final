0d08f7619707db04f301ea1c07fdda4991258f69
Cory Risch-Smith <coryrischsmith1201@gmail.com>
2021-06-01 10:08:05 -0700

    debugged Score, created game over function

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-//To-Do: 
+//To-Do:
 // 1. Debug Score
 // 2. Fix weird styling
 // 3. Populate game array objects with Kinu's stuff
@@ -6,7 +6,7 @@
 // 5. Game end screen
 
 const optionA = document.getElementById('option-a');
-const optionB = document.getElementById('option-b'); 
+const optionB = document.getElementById('option-b');
 const optionC = document.getElementById('option-c');
 const optionD = document.getElementById('option-d');
 const optionE = document.getElementById('option-e');
@@ -22,85 +22,118 @@ const correctAnswer = document.getElementById('correct-answer');
 const overlayBg = document.getElementById('overlayBackground');
 // var options = document.getElementsByClassName('option');
 
-
 const game = [
-    {image: "https://picsum.photos/300/200",
-    question: "What do you want to do today?",
-    options: ["location", "alphabet", "time", "category", "hierarchy"],
-    correctAns: "location"}, 
+  {
+    image: 'https://picsum.photos/300/200',
+    question: 'What do you want to do today?',
+    options: ['location', 'alphabet', 'time', 'category', 'hierarchy'],
+    correctAns: 'location',
+  },
 
-    {image: "https://picsum.photos/300/200",
-    question: "This is question 2",
-    options: ["location and hierarchy", "alphabet", "time", "category", "just hierarchy"],
-    correctAns: "alphabet"}, 
+  {
+    image: 'https://picsum.photos/300/200',
+    question: 'This is question 2',
+    options: [
+      'location and hierarchy',
+      'alphabet',
+      'time',
+      'category',
+      'just hierarchy',
+    ],
+    correctAns: 'alphabet',
+  },
 
-    {image: "https://picsum.photos/300/200",
-    question: "How many elephants are in this pic?",
-    options: ["location and hierarchy", "alphabet", "time", "category", "just hierarchy"],
-    correctAns: "time"}, 
-]
-var score = 0;
+  {
+    image: 'https://picsum.photos/300/200',
+    question: 'How many elephants are in this pic?',
+    options: [
+      'location and hierarchy',
+      'alphabet',
+      'time',
+      'category',
+      'just hierarchy',
+    ],
+    correctAns: 'time',
+  },
+];
+
+var score = -1;
+
 var i = 0;
 
 overlayMessage.style.display = 'none';
 
-function playGame(){
-    //Question 1
-    
-    for (let option of document.getElementsByClassName('option')) {
-        option.addEventListener('click', function() {
-            console.log("Option clicked");
-            if (this.textContent === game[i].correctAns){
-                //Turn on Success overlay
-                maingame.style.display = 'none'; 
-                overlayMessage.style.display = 'flex';
-                overlayBg.style.display = 'block';
-                message.textContent = 'Congratulations!';
-                message.style.color = 'green';
-                overlayText.textContent = 'You are great! No studying for you!';
-                correctAnswer.textContent = `The correct LATCH principle is ${game[i].correctAns}.`
-                score++;
-                scoreLabel.textContent = `Score: ${score}`;
-            } else {
-                // Turn on failure overlay
-                maingame.style.display = 'none';
-                overlayMessage.style.display = 'flex';
-                overlayBg.style.display = 'block';
-                message.textContent = 'Wrong answer, please try again!';
-                message.style.color = 'red';
-                correctAnswer.textContent = `The correct LATCH principle is ${game[i].correctAns}.`
-                overlayText.textContent = 'You are so close, please go study more.';
-                scoreLabel.textContent = `Score: ${score}`;
-            }
-        });
-    }
+function playGame() {
+  //Question 1
 
-    // when proceed is clicked, increment i and display the new page.
-    nextPage.addEventListener('click', function() {
-        overlayMessage.style.display = 'none';
-        maingame.style.display = 'flex';
-        if (i == game.length - 1) {
-            return;
-            //Where we call end of game page. 
-        }
-        i++;
-        pageDisplay(i);
-        playGame();
+  for (let option of document.getElementsByClassName('option')) {
+    option.addEventListener('click', function () {
+      console.log('Option clicked');
+      if (this.textContent === game[i].correctAns) {
+        //Turn on Success overlay
+        maingame.style.display = 'none';
+        overlayMessage.style.display = 'flex';
+        overlayBg.style.display = 'block';
+        message.textContent = 'Congratulations!';
+        message.style.color = 'green';
+        overlayText.textContent = 'You are great! No studying for you!';
+        correctAnswer.textContent = `The correct LATCH principle is ${game[i].correctAns}.`;
+      } else {
+        // Turn on failure overlay
+        maingame.style.display = 'none';
+        overlayMessage.style.display = 'flex';
+        overlayBg.style.display = 'block';
+        message.textContent = 'Wrong answer, please try again!';
+        message.style.color = 'red';
+        correctAnswer.textContent = `The correct LATCH principle is ${game[i].correctAns}.`;
+        overlayText.textContent = 'You are so close, please go study more.';
+        scoreLabel.textContent = `Score: ${score}`;
+      }
     });
+    if (option.textContent === game[i].correctAns) {
+      score++;
+      scoreLabel.textContent = `Score: ${score}`;
+    }
+  }
 
+  // when proceed is clicked, increment i and display the new page.
+  nextPage.addEventListener('click', function () {
+    overlayMessage.style.display = 'none';
+    maingame.style.display = 'flex';
+    if (i == game.length - 1) {
+      endGame();
+      //Where we call end of game page.
+    }
+    i++;
+    pageDisplay(i);
+    playGame();
+  });
+}
+
+function endGame() {
+  maingame.style.display = 'none';
+  overlayMessage.style.display = 'flex';
+  overlayBg.style.display = 'block';
+  scoreLabel.textContent = `Score: ${score}`;
+
+  //Message
+  message.textContent = 'GAME OVER!';
+  message.style.color = 'orange';
+  overlayText.textContent = `Congratulations, you scored ${score} points! Would you like to play again?`;
+
+  //   Add start and end buttons that refresh game
 }
 
 function pageDisplay(pageIndex) {
-    overlayBg.style.display = 'none';
-    gameImage.setAttribute("src", game[pageIndex].image);
-    question.textContent = game[pageIndex].question;
-    optionA.textContent = game[pageIndex].options[0];
-    optionB.textContent = game[pageIndex].options[1];
-    optionC.textContent = game[pageIndex].options[2];
-    optionD.textContent = game[pageIndex].options[3];
-    optionE.textContent = game[pageIndex].options[4];
+  overlayBg.style.display = 'none';
+  gameImage.setAttribute('src', game[pageIndex].image);
+  question.textContent = game[pageIndex].question;
+  optionA.textContent = game[pageIndex].options[0];
+  optionB.textContent = game[pageIndex].options[1];
+  optionC.textContent = game[pageIndex].options[2];
+  optionD.textContent = game[pageIndex].options[3];
+  optionE.textContent = game[pageIndex].options[4];
 }
 
 pageDisplay(i);
 playGame();
-
